f027c924bdc7295ca8ffc6153e717ae06fb17634
JoseZay <josezay22@gmail.com>
2022-02-09 16:43:49 -0300

    -Separadas as funções gerais para arquivo Biblio;

--- biblio.pas
+++ biblio.pas
@@ -5,13 +5,373 @@ unit Biblio;
 interface
 
 uses
-  Classes, SysUtils, DCPsha256;
+  Classes, SysUtils, DCPsha256, Dialogs, process, fphttpclient, FileUtil;
 
   function sha256(S: String): String;
+  function valida(Tipo: integer): boolean;
+  function geraRAR(Matricula: string): boolean;
+  function geraPDF(Numero: string; Tipo: integer): boolean;
+  function sincronizaArquivo(Numero: string; Tipo: integer): boolean;
+  function geraTIF(Matricula: string): boolean;
+  function apagaArquivosOrigem(): boolean;
 
 implementation
 
 uses form_principal;
+
+//********** Regra Negocial ****************************************************
+
+// Validações
+// Tipo : 2 se matrícula, 3 se auxiliar
+function valida(Tipo: integer): boolean;
+var
+    I: integer;
+begin
+    // Validações gerais;
+    valida := true;
+    if (Length(Imagens) = 0) then                                               // Se não ouverem imagens carregadas.
+    begin
+        MessageDlg('É necessário escolher ao menos uma imagem!', mtError, mbOKCancel, 0);
+        if Principal.DialogoImagens.Execute then                                          // Se arquivos foram escolhidos.
+        begin
+            Principal.ListaArquivos.Items.Clear;                                          // Limpa na tela a lista.
+            SetLength(Imagens, Principal.DialogoImagens.Files.Count);                     // Define o tamanho da array de imagens para que comporte as imagens escolhidas.
+            for I := 0 to Principal.DialogoImagens.Files.Count - 1 do                     // Para cada arquivo escolhido.
+            begin
+                Imagens[I] := Principal.DialogoImagens.Files[I];                          // Adicina o arquivo na lista para ser processado.
+                Principal.ListaArquivos.items.add(ExtractFileName(Principal.DialogoImagens.Files[I])); // Mostra o nome simples na tela.
+            end;
+            valida := false;
+            Exit;
+        end;
+    end;
+
+    // Validações específicas
+    if (Tipo = 2) then
+    begin
+        if (Principal.CampoNumeroMatricula.Text = '') then
+        begin
+            MessageDlg('Preencha o número da matrícula!', mtError, [mbOK], 0);
+            Principal.CampoNumeroMatricula.SetFocus;
+            valida := false;
+            Exit;
+        end;
+
+        if (Principal.FormStorage.StoredValue['DiretorioRARMatricula'] = '') then
+        begin
+            MessageDlg('É necessário escolher o diretório de destino para arquivos RAR!', mtError, [mbOK], 0);
+            valida := false;
+            Exit;
+        end;
+
+        if (Principal.FormStorage.StoredValue['DiretorioPDFMatricula'] = '') then
+        begin
+            MessageDlg('É necessário escolher o diretório de destino para arquivos PDF!', mtError, [mbOK], 0);
+            valida := false;
+            Exit;
+        end;
+
+        if (Principal.FormStorage.StoredValue['DiretorioTIFMatricula'] = '') then
+        begin
+            MessageDlg('É necessário escolher o diretório de destino para arquivos TIF!', mtError, [mbOK], 0);
+            valida := false;
+            Exit;
+        end;
+    end
+    else
+    begin
+        if (Tipo = 3) then
+        begin
+            if (Principal.CampoNumeroAuxiliar.Text = '') then
+            begin
+                MessageDlg('Preencha o número do Registro Auxiliar!', mtError, [mbOK], 0);
+                Principal.CampoNumeroAuxiliar.SetFocus;
+                valida := false;
+                Exit;
+            end;
+
+            if (Principal.FormStorage.StoredValue['DiretorioPDFAuxiliar'] = '') then
+            begin
+                MessageDlg('É necessário escolher o diretório de destino para arquivos PDF!', mtError, [mbOK], 0);
+                valida := false;
+                Exit;
+            end;
+        end;
+    end;
+end;
+
+// Compacta arquivos
+// Somente tipo 2 gera RAR
+function geraRAR(Matricula: string): boolean;
+var
+    RunProgram: TProcess;
+    I: integer;
+begin
+    RunProgram := TProcess.Create(nil);
+    RunProgram.Executable := 'bin/rar.exe';
+    RunProgram.Parameters.Add('a');                                             // Compactar
+    RunProgram.Parameters.Add('-ep1');                                          // Sem manter estrutura de arquivos
+    RunProgram.Parameters.Add('"' + Principal.FormStorage.StoredValue['DiretorioRARMatricula'] + '/' + Matricula + '.rar"');
+
+    for I := Low(Imagens) to High(Imagens) do
+    begin
+        RunProgram.Parameters.Add(Imagens[I]);
+    end;
+
+    RunProgram.Options := RunProgram.Options + [poWaitOnExit];
+    RunProgram.ShowWindow := TShowWindowOptions.swoHIDE;                        // Para que não apareça a tela preta.
+    RunProgram.Execute;
+
+    if (RunProgram.ExitCode = 0) then geraRAR:=true                             // Se ouve erro ao executar processo externo.
+    else geraRAR:=false;
+
+    RunProgram.Free;
+end;
+
+// Gera PDF-A
+// Numero: Nome do PDF
+// Tipo: 2 se matrícula, 3 se auxiliar
+function geraPDF(Numero: string; Tipo: integer): boolean;
+var
+    RunProgram: TProcess;
+    I: integer;
+begin
+    // Gera PDF normal temporário
+    RunProgram := TProcess.Create(nil);
+    RunProgram.Executable := 'magick';
+
+    for I := Low(Imagens) to High(Imagens) do
+    begin
+        RunProgram.Parameters.Add(Imagens[I]);
+    end;
+
+    RunProgram.Parameters.Add(Numero + '.pdf');
+    RunProgram.Options := RunProgram.Options + [poWaitOnExit];
+    RunProgram.ShowWindow := TShowWindowOptions.swoHIDE;                        // Para que não apareça a tela preta.
+    RunProgram.Execute;
+    RunProgram.Free;
+    //ShowMessage(RunProgram.Output.Read());
+
+    // Gera PDFA
+    RunProgram := TProcess.Create(nil);
+    RunProgram.Executable := 'bin\gswin32c.exe';
+    RunProgram.Parameters.Add('-dPDFA=1');
+    RunProgram.Parameters.Add('-dNOSAFER');
+    RunProgram.Parameters.Add('-dBATCH');
+    RunProgram.Parameters.Add('-dNOPAUSE');
+    RunProgram.Parameters.Add('-sDEVICE=pdfwrite');
+    RunProgram.Parameters.Add('-sProcessColorModel=DeviceRGB');
+    RunProgram.Parameters.Add('-sColorConversionStrategy=RGB');
+    RunProgram.Parameters.Add('-dDOPDFMARKS=false');
+    RunProgram.Parameters.Add('-dCompatibilityLevel=1.7');
+    RunProgram.Parameters.Add('-dPDFACompatibilityPolicy=2');
+
+    if (Tipo = 2) then
+    begin
+        RunProgram.Parameters.Add('-sOutputFile=' + '"' + Principal.FormStorage.StoredValue['DiretorioPDFMatricula'] + '\' + Numero + '.pdf"');
+    end;
+
+    if (Tipo = 3) then
+    begin
+        RunProgram.Parameters.Add('-sOutputFile=' + '"' + Principal.FormStorage.StoredValue['DiretorioPDFAuxiliar'] + '\' + Numero + '.pdf"');
+    end;
+
+    RunProgram.Parameters.Add('bin\PDFA_def.ps');
+    RunProgram.Parameters.Add(Numero + '.pdf');
+    RunProgram.Options := RunProgram.Options + [poWaitOnExit];
+    RunProgram.ShowWindow := TShowWindowOptions.swoHIDE;                        // Para que não apareça a tela preta.
+    RunProgram.Execute;
+
+    if (RunProgram.ExitCode = 0) then geraPDF:=true                             // Se ouve erro ao executar processo externo.
+    else geraPDF:=false;
+
+    RunProgram.Free;
+
+    sincronizaArquivo(Numero, Tipo);                                            // Sincroniza arquivo PDF-A com servidor
+
+    if (FileExists(Numero + '.pdf')) then
+    begin
+        DeleteFile(Numero + '.pdf')                                             // Deleta PDF normal temporário.
+    end;
+end;
+
+// Sincroniza um arquivo para o servidor
+function sincronizaArquivo(Numero: string; Tipo: integer): boolean;
+var
+    Respo: TStringStream;
+    S, Arquivo: string;
+begin
+    if (Tipo = 2) then
+    begin
+        Arquivo := StringReplace(Principal.FormStorage.StoredValue['DiretorioPDFMatricula'], '\', '/', [rfReplaceAll]) + '/' + Numero + '.pdf';
+    end;
+
+    if (Tipo = 3) then
+    begin
+        Arquivo := StringReplace(Principal.FormStorage.StoredValue['DiretorioPDFAuxiliar'], '\', '/', [rfReplaceAll]) + '/' + Numero + '.pdf';
+    end;
+
+    With TFPHttpClient.Create(Nil) do
+    try
+        try
+            Respo := TStringStream.Create('');
+            FileFormPost(Principal.ConfigStorage.StoredValue['DiretorioRemoto'] + 'notaire_image.php?token=' + sha256(Numero + '.pdf' + Principal.ConfigStorage.StoredValue['Senha']) + '&tipo=' + IntToStr(Tipo),
+                         'file',
+                         Arquivo,
+                         Respo);
+            S := Respo.DataString;
+            Respo.Destroy;
+        except
+            Principal.BarraDeStatus.SimpleText := 'Sem conexão com a internet';
+        end;
+    finally
+        if (S = '1') then                                                       // Se sucesso.
+        begin
+            Principal.BarraDeStatus.SimpleText := '';
+            sincronizaArquivo := true;                                          // Sincroniza o arquivo PDF-A original.
+        end
+        else
+        begin
+            if (Tipo = 2) then
+            begin
+                CreateDir(Principal.ConfigStorage.StoredValue['DiretorioPendencias'] + '/matriculas/');
+                CopyFile(Arquivo, Principal.ConfigStorage.StoredValue['DiretorioPendencias'] + '/matriculas/' + Numero + '.pdf');   // Copia o arquivo original na pasta pendentes.
+            end;
+
+            if (Tipo = 3) then
+            begin
+                CreateDir(Principal.ConfigStorage.StoredValue['DiretorioPendencias'] + '/auxiliares/');
+                CopyFile(Arquivo, Principal.ConfigStorage.StoredValue['DiretorioPendencias'] + '/auxiliares/' + Numero + '.pdf');
+            end;
+
+            sincronizaArquivo := false;
+        end;
+    end;
+end;
+
+// Ressincroniza arquivos pendentes
+//function TPrincipal.ressincronizaArquivos(): boolean;
+//var
+//    ArquivosPendentes: TStringList;
+//    I: integer;
+//begin
+//    ArquivosPendentes := TStringList.Create;
+//    try
+//        if (ConfigStorage.StoredValue['Ressincroniza'] = 'true') then
+//        begin
+//            FindAllFiles(ArquivosPendentes, 'matriculas_pendentes', '*.pdf', true);
+//            if (ArquivosPendentes.Count > 0) then
+//            begin
+//                ShowMessage(Format('Encontradas %d matricula(s) não sincronizada(s)', [ArquivosPendentes.Count]));
+//                for I := 0 to ArquivosPendentes.Count - 1 do
+//                begin
+//                     sincronizaArquivo(ArquivosPendentes[I], 2);
+//                end;
+//            end;
+//
+//            FindAllFiles(ArquivosPendentes, 'auxiliares_pendentes', '*.pdf', true);
+//            if (ArquivosPendentes.Count > 0) then
+//            begin
+//                ShowMessage(Format('Encontrados %d registros auxiliar(es) não sincronizado(s)', [ArquivosPendentes.Count]));
+//                for I := 0 to ArquivosPendentes.Count - 1 do
+//                begin
+//                     sincronizaArquivo(ArquivosPendentes[I], 3);
+//                end;
+//            end;
+//        end;
+//    finally
+//        ressincronizaArquivos := true;
+//    end;
+//end;
+
+// Gera TIF
+function geraTIF(Matricula: string): boolean;
+var
+    I: integer;
+    SubdiretorioTIF, NomeTIF: string;
+    RunProgram: TProcess;
+begin
+    // Gera diretório
+    SubdiretorioTIF := '00000000';                                              // Caso não entre no if abaixo
+    if (Matricula.Length > 3) then
+    begin
+        SubdiretorioTIF := '';
+        for I := 1 to Matricula.Length - 3 do
+        begin
+            SubdiretorioTIF := SubdiretorioTIF + Matricula[I];
+        end;
+
+        NomeTif := '';                                                          // Usa o nometif como temporário somente
+        for I := SubdiretorioTIF.Length to 7 do
+        begin
+            NomeTIF := NomeTIF + '0';
+        end;
+
+        SubdiretorioTIF := NomeTIF + SubdiretorioTIF;
+    end;
+
+    if not DirectoryExists(Principal.FormStorage.StoredValue['DiretorioTIFMatricula'] + '/' + SubdiretorioTIF) then
+    begin
+        if not CreateDir(Principal.FormStorage.StoredValue['DiretorioTIFMatricula'] + '/' + SubdiretorioTIF) then
+        begin
+            MessageDlg('Falha ao criar subdiretório Tif, crie manualmente uma pasta de nome ' + SubdiretorioTIF + ' dentro de ' + Principal.FormStorage.StoredValue['DiretorioTIF'], mtError, mbOKCancel, 0);
+            geraTIF := false;
+        end;
+    end;
+
+    // Gera nome com 0MenuItemSair à esquerda
+    NomeTIF := '';
+
+    for I := Matricula.Length to 7 do
+    begin
+         NomeTIF := NomeTIF + '0';
+    end;
+
+    NomeTIF := NomeTIF + Matricula;
+
+    // Converte para TIF
+    RunProgram := TProcess.Create(nil);
+    RunProgram.Executable := 'magick';
+
+    for I := Low(Imagens) to High(Imagens) do
+    begin
+        RunProgram.Parameters.Add(Imagens[I]);
+    end;
+
+    if (Principal.ConfigStorage.StoredValue['ComprimirTIF'] = 'true') then                // Comprime o tif (preto e branco) se marcado para tal na configuração.
+    begin
+        RunProgram.Parameters.Add('-compress');
+        RunProgram.Parameters.Add('group4');
+    end;
+
+    RunProgram.Parameters.Add('"' + Principal.FormStorage.StoredValue['DiretorioTIFMatricula'] + '/' + SubdiretorioTIF + '/' + NomeTIF + '.tif');
+    RunProgram.Options := RunProgram.Options + [poWaitOnExit];
+    RunProgram.ShowWindow := TShowWindowOptions.swoHIDE;                        // Para que não apareça a tela preta.
+    RunProgram.Execute;
+
+    if (RunProgram.ExitCode = 0) then geraTIF:=true                             // Se ouve erro ao executar processo externo.
+    else geraTIF:=false;
+
+    RunProgram.Free;
+end;
+
+// Apaga arquivos de origem
+function apagaArquivosOrigem(): boolean;
+var
+    I: integer;
+begin
+    for I := Low(Imagens) to High(Imagens) do
+    begin
+        if (FileExists(Imagens[I])) then
+        begin
+            DeleteFile(Imagens[I])
+        end;
+    end;
+
+    Principal.ListaArquivos.Clear;
+    SetLength(Imagens, 0);
+    apagaArquivosOrigem := true;
+end;
 
 function sha256(S: String): String;
 var
